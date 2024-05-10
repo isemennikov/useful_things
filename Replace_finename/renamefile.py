4875d5b4ce13f1bae3b12cc517953fdb8e9a8e02
Ilay Semennikov <semennikovia@gmail.com>
2024-05-11 00:40:45 +0300

    light file

--- Replace_finename/renamefile.py
+++ Replace_finename/renamefile.py
@@ -2,6 +2,9 @@ import os
 import re
 import sys
 
+import os
+import re
+
 def rename_files(directory):
     files = os.listdir(directory)
     renamed_files = []
@@ -11,14 +14,13 @@ def rename_files(directory):
             file_type = '.pdf' if file.lower().endswith('.pdf') else '.epub'
             new_name = file
 
-            # Проверка на наличие 'dokumen.pub_' и последовательности из чисел и дефисов
-            if 'dokumen.pub_' in file or re.search(r'[-\d]{4,12}', file):
-                new_name = new_name.replace('dokumen.pub_', '')
-                new_name = re.sub(r'[-\d]{{8,12}}(?=\.{})'.format(re.escape(file_type)), '', new_name)
-            else:
-                # Если условия не совпадают, просто заменяем пробелы и нежелательные символы
-                new_name = new_name.replace(' ', '_')
-                new_name = re.sub(r'[\/\'\\]', '-', new_name)
+            # Удаление 'dokumen.pub_' и последовательности цифр и дефисов перед расширением файла
+            new_name = new_name.replace('dokumen.pub_', '')
+            new_name = re.sub(r'[-\d]{8,12}(?=\.{})'.format(re.escape(file_type)), '', new_name)
+
+            # Замена пробелов на подчеркивания и удаление нежелательных символов
+            new_name = new_name.replace(' ', '_')
+            new_name = re.sub(r'[\/\'\\]', '-', new_name)
 
             # Переименование файла, если оно необходимо
             if new_name != file:
